04b5cd530a426f0eb174f40e7e4ce51bb8cad513
Umut Can <96837086+UmutC4nORAL@users.noreply.github.com>
2024-05-16 15:22:47 +0300

    Update MainGame.cs
    Zar atma eklendi. Aktarılan başlangıç parası ve oyuncu sayısı alındı.

--- MonoForms/MonoForms/MainGame.cs
+++ MonoForms/MonoForms/MainGame.cs
@@ -12,9 +12,35 @@ namespace MonoForms
 {
     public partial class MainGame : Form
     {
-        public MainGame()
+        private int startingMoney;
+        private int playerCount;
+        private Random random = new Random();
+        public MainGame(int startingMoney, int playerCount)
         {
             InitializeComponent();
+            this.startingMoney = startingMoney;
+            this.playerCount = playerCount;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            zar atma butonu
+            // İki zar için rastgele sayılar üret
+            int die1 = random.Next(1, 7); // 1 ile 6 arası rastgele sayı
+            int die2 = random.Next(1, 7); // 1 ile 6 arası rastgele sayı
+
+            pictureBox2.Image = Image.FromFile($"D:/görsel programlama/MonoForms/MonoForms/MonoForms/Assets/Die/die{die1}.jpg"); // Bilgisayarımdaki dosya yolları bozuk olduğu için kendi bilgisayarımda çalışan halini ekledim.
+            pictureBox3.Image = Image.FromFile($"D:/görsel programlama/MonoForms/MonoForms/MonoForms/Assets/Die/die{die2}.jpg");
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            //zar1 için pb
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            //zar2 için pb
         }
     }
 }
