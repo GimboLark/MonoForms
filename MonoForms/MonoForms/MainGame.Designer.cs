aaa4cfca1c6b53464c3103c77b1f95c4e4a29789
Umut Can <96837086+UmutC4nORAL@users.noreply.github.com>
2024-05-16 15:38:34 +0300

    Update MainGame.Designer.cs

--- MonoForms/MonoForms/MainGame.Designer.cs
+++ MonoForms/MonoForms/MainGame.Designer.cs
@@ -20,6 +20,10 @@
             base.Dispose(disposing);
         }
 
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        
         #region Windows Form Designer generated code
 
         /// <summary>
